d6a816f9ccfdb412bcc05d7c3ee0f69565e1a65f
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-19 01:41:49 -0600

    Configuracion inicial del servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('My app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Ruta del servidor
+app.get("/", (req, res) => {
+  res.send("Hola mi server en Express");
+});
+
+//Levantamos el puerto
+app.listen(port, () => {
+  console.log("Listening at http://localhost:" + port);
+})
+
+
