00bd984c1df1a51e8d469fb723c2ecb0e494e46c
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-19 02:18:32 -0600

    Se configuran endpoints con parametros y queryparams

--- index.js
+++ index.js
@@ -7,9 +7,65 @@ app.get("/", (req, res) => {
   res.send("Hola mi server en Express");
 });
 
+app.get("/new", (req, res) => {
+  res.send("Hola soy una nueva ruta");
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json([
+    {
+      name: 'Triciclo',
+      price: 3000
+    },
+    {
+      name: 'Balón',
+      price: 800
+    }
+  ]);
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+
+  const { id } = req.params;
+
+  res.json(
+    [
+      {
+        id: id,
+        name: 'Balón',
+        price: 800
+      }
+    ]
+  );
+});
+
+app.get('/api/v1/categories/:categoryId/products/:productId', (req, res) =>{
+  //Recibimos los parametros
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId
+  });
+})
+
+app.get('/api/v1/user', (req, res) => {
+  const { limit, offset } = req.query;
+
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  }else{
+    res.send('No hay parametros');
+  }
+})
+
+
 //Levantamos el puerto
 app.listen(port, () => {
   console.log("Listening at http://localhost:" + port);
-})
+});
 
 
